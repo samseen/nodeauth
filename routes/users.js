e84645b1a00c1aec248c3ddb29292005a9121bcb
Samson Akanbi <samson.akanbi@outlook.com>
2018-11-22 23:43:55 +0100

    Added passport for the login credentials but it still doesn't work as expected

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var multer = require('multer');
 var upload = multer({dest: './uploads'});
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -18,6 +20,40 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
 });
 
+router.post('/login',
+  passport.authenticate('local',{failureRedirect:'/users/login', failureFlash: 'Invalid Username or Password'}),
+  function(req, res) {
+    req.flash('success', 'You are now logged in');
+    res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done){
+  User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+      return done(null, false, {message: 'Unknown User'});
+    }
+
+    User.comparePassword(password, user.password, function(err, isMatch){
+      if(err) return done(err);
+      if(isMatch){
+        return done(null, user);
+      } else {
+        return done(null, false, {message: 'Invalid Password'});
+      }
+    });
+  });
+}));
 
 router.post('/register', upload.single('profileimage'),function(req, res, next) {
   var name = req.body.name;
